62b0f8e0d40ab83e52f4d715a5dbdece62ba0cd9
Ice-Coffey <38543752+Ice-Coffey@users.noreply.github.com>
2018-11-03 16:31:05 -0700

    New allsides Scraper

---
+++ allSidesScraper4.py
@@ -0,0 +1,58 @@
+import datetime
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import socket
+import requests
+import urllib2
+import justext
+import string
+
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--ignore-ssl-errors')
+options.add_argument("headless")
+options.add_argument('--disable-gpu')
+options.add_argument("--log-level=3")
+driver = webdriver.Chrome(chrome_options=options)
+
+def printable(line):
+	if (line == None):
+		return ''
+	filtered_string = filter(lambda x: x in string.printable, line)
+	line = "".join(filtered_string)
+	return line
+
+def scrapeArticle(listOfAllsideUrls):
+	articleDict = dict()
+	for url2 in listOfAllsideUrls:
+		date = None
+		try:
+			date = url2.split('/')[4].split('-')
+			date = date[1]+'/'+date[2]+'/'+date[0]
+			print(date)
+		except:
+			pass
+		l = list()
+		try:
+			driver.get(url2)
+			python_button = driver.find_elements_by_class_name("open-new-page")
+			python_button[0].click()
+			url = driver.current_url
+			response = requests.get(url)
+			paragraphs = justext.justext(response.content, justext.get_stoplist("English"))
+			for paragraph in paragraphs:
+				if (not (paragraph.is_boilerplate) and (len(paragraph.text)>30)):
+					l.append(printable(paragraph.text.encode('ascii','ignore')))
+
+					if('...'in paragraph.text):
+						break
+			Article = '\n'.join(l)
+			charCount = len(Article)
+			wordCount = len(Article.split())
+			articleDict[url2] = (Article,date,wordCount,charCount)
+			print(articleDict[url2])
+		except:
+			continue
+	return articleDict
+
+driver.close()
